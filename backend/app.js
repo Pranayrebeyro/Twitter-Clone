a4e22dc999bd8dede81859802b60ffb62cce2bb6
Pranayrebeyro <pranayrebeyro@gmail.com>
2026-07-04 12:15:58 +0530

    Fix CORS

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,12 @@ const cors = require("cors");
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 app.options("*", cors());
 app.use(express.json());
 
